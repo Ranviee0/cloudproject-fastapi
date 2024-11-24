fe555e0caf63310b037e700699affecf16013087
ranvie <neodyte@gmail.com>
2024-11-24 17:29:02 +0700

    database.py

--- database.py
+++ database.py
@@ -1,17 +1,19 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-username = "admin"
-password = "group9login"
-host = "localhost"
-port = 3307
-database = "CCTV_service"
+# Read environment variables for database configuration
+username = os.getenv("DB_USERNAME", "admin")
+password = os.getenv("DB_PASSWORD", "group9login")
+host = os.getenv("DB_HOST", "localhost")
+port = os.getenv("DB_PORT", "3306")
+database = os.getenv("DB_NAME", "CCTV_service")
 
+# Database URL
 URL_DATABASE = f"mysql+pymysql://{username}:{password}@{host}:{port}/{database}"
 
+# Create engine and session
 engine = create_engine(URL_DATABASE)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
